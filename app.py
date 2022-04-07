25c7541c0e20ed5be3a70fe011605481a1120e16
jimmyEverguard <jimmy.ni@everguard.ai>
2022-04-07 21:30:48 +0000

    init 4/7

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, jsonify
+import json
+import subprocess
+import time
+from subprocess import PIPE, run
+
+app = Flask(__name__)
+
+default_json = ["bkup_configs/irvine_2ppe.json","bkup_configs/irvine_bk.json"]
+
+@app.route('/command', methods=['POST'])
+def command_recv():
+    # ======================================================== #
+    # Command Key
+    # run: 
+    #   type: watchdog / stream
+    #   json: name of the json file
+    # stop
+    # update
+    # ======================================================== #
+    command = json.loads(request.data)
+    
+    if "run" in command:
+        res = run_pipeline(command["run"])
+        return "Error" if not res else "Succeed"
+    elif "stop" in command:
+        stop_pipeline()
+    elif "update" in command:
+        update_pipeline(command["update"])
+    elif "check" in command:
+        result = check_pipeline()
+        return str(result)
+    return '200'
+
+def run_pipeline(command):
+    if "type" not in command.keys() or "path" not in command.keys():
+        return False
+    if "json" not in command.keys():
+        for cfg in default_json:
+            subprocess.run(["./docker-run.sh",command["type"],cfg,"--opt","background"],cwd=command["path"])
+            time.sleep(30)
+    else:
+        for cfg in command["json"]:
+            #print(command["json"])
+            subprocess.run(["./docker-run.sh",command["type"],cfg,"--opt","background"],cwd=command["path"])
+            time.sleep(30)
+    return True
+
+def stop_pipeline():
+    # Current design stop all docker image in the server
+    # docker stop $(docker ps -q)
+    bytes = subprocess.run(["docker","ps","-q"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    str =bytes.stdout.decode("utf-8")
+    ids = str.split('\n')[:-1]
+    for id in ids:
+        subprocess.run(["docker","stop",id])
+
+def update_pipeline(command):
+    
+    sys_monitor_path = command["sys_monitor_path"]
+    eg_pipeline_path = command["eg_pipeline_path"]
+    #print(sys_monitor_path,eg_pipeline_path)
+    # sys_monitor git pull, git submodule updat, docker build
+    subprocess.run(["git","pull"],cwd=sys_monitor_path)
+    subprocess.run(["git","submodule","update","--recursive","--remote"],cwd=sys_monitor_path)
+    subprocess.run(["./docker-build.sh"],cwd=sys_monitor_path)
+    time.sleep(60)
+    # eg_pipeline git pull, git submodule updat, docker build
+    subprocess.run(["git","pull"],cwd=eg_pipeline_path)
+    subprocess.run(["git","submodule","update","--recursive","--remote"],cwd=eg_pipeline_path)
+    subprocess.run(["./docker-build.sh"],cwd=eg_pipeline_path)
+    time.sleep(60)
+    
+
+def check_pipeline():
+    bytes = subprocess.run(["docker","ps"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    str =bytes.stdout.decode("utf-8")
+    lines = str.split('\n')[1:-1]
+    running_image = 0
+    for line in lines:
+        field = line.split(' ')
+        for i in range(len(field)):
+            if field[i]== "eg/eg-pipeline":
+                running_image += 1
+    return running_image
+
+
+if __name__ == '__main__':
+    
+    app.run(host='0.0.0.0', port=5502)
---
+++ send_command.py
@@ -0,0 +1,93 @@
+import json
+import requests
+import argparse
+import sys
+
+def get_POST_data(args,dic):
+    data = None
+    
+    if args.command == 'stream':
+        data = json.dumps({'run':{"type":"stream","json":dic["default_cfg"],"path":dic["default_path"]}})
+    elif args.command == 'watchdog':
+        data = json.dumps({'run':{"type":"watchdog","json":dic["default_cfg"],"path":dic["default_path"]}})
+    elif args.command == 'check':
+        data = json.dumps({"check":True})
+    elif args.command == 'update':
+        data = json.dumps({"update":{"eg_pipeline_path":dic["eg_pipeline_path"],"sys_monitor_path":dic["sys_monitor_path"]}})
+    elif args.command == 'stop':
+        data = json.dumps({"stop":True})
+        
+    return data
+
+
+def check_command(args):
+    if args.command == None:
+        print("No Command Found") 
+        sys.exit(0)
+    else:
+        if args.command not in ['stream','check','watchdog','update','stop']:
+            print("Command Name Error, Must be stream, watchdog, update, stop, check")
+            sys.exit(0)
+
+                
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument('-s','--servers', type=str, nargs='+')
+    p.add_argument('-c','--command', type=str, default='check')
+    
+    # read the command and the cfg file
+    args = p.parse_args()
+    #print(args.command,args.servers)
+    with open("servers.json","r") as f:
+        cfg = json.load(f)
+         
+    checking = {}    
+    run_command_check = {}
+    
+    check_command(args)
+    print("Command Accepted")
+    # decide the target
+    if args.servers == None:
+        for k in cfg.keys():
+            server = cfg[k]
+            data = get_POST_data(args,server)
+            url = 'http://' + server["ip"] + ':' + server["port"] + '/command'
+            print("Request send to:", server["ip"])
+            print("Command:", args.command)
+            response = requests.post(url, data = data)
+            if args.command == 'check':
+                running = response.content.decode("utf-8")
+                checking[k] = [len(server["default_cfg"]),running]
+            if args.command == 'stream' or args.command == 'watchdog':
+                status = response.content.decode("utf-8")
+                run_command_check[k] = status
+            
+    else:
+        for server_name in args.servers:
+            server = cfg[server_name]
+            data = get_POST_data(args,server)
+            url = 'http://' + server["ip"] + ':' + server["port"] + '/command'
+            print("Request send to:", server["ip"])
+            print("Command:", args.command)
+            response = requests.post(url, data = data)
+            if args.command == 'check':
+                running = response.content.decode("utf-8")
+                checking[server_name] = [len(server["default_cfg"]),running]
+            if args.command == 'stream' or args.command == 'watchdog':
+                status = response.content.decode("utf-8")
+                run_command_check[server_name] = status
+    
+    if args.command == 'check':
+        for k in checking.keys():
+            print("In Server: ",k)
+            print("# Should Run: ", checking[k][0])
+            print("# Current Running: ",checking[k][1])
+            
+    if args.command == 'stream' or args.command == 'watchdog':
+        for k in run_command_check.keys():
+            print("In Server: ",k)
+            print("Status: ", run_command_check[k])
+        
+        
+    
